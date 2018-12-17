7031b8ab88c00b941640307422c8aa2d0b269a71
Theron Luhn <theron@luhn.com>
2018-12-16 16:26:05 -0800

    Migrate to Poetry.

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
 
-init:
-	pipenv install --dev
-	pipenv run python setup.py develop
+lint:
+	poetry run flake8 pyramid_apispec tests
 
-lint: init
-	pipenv run flake8 pyramid_apispec tests
+test: lint
+	poetry run pytest tests/
 
-test: init lint
-	pipenv run py.test tests/
+.PHONY: lint test
--- setup.py
+++ setup.py
@@ -1,30 +1,10 @@
 from setuptools import setup, find_packages
 
 
-REQUIRES = [
-    'pyramid>=1.7,<2',
-    'marshmallow>=2.0,<3',
-    'apispec>=0.37,<0.38',
-]
-
-
 setup(
     name='pyramid-marshmallow',
     version='0.4.0',
-    description='TODO',
-    long_description=open('README.rst').read(),
-    license='MIT',
-    author='Theron Luhn',
-    author_email='theron@luhn.com',
-    url='https://github.com/luhn/pyramid-marshmallow',
-    packages=find_packages(),
     package_data={
         'pyramid_marshmallow': ['assets/*'],
-    },
-    install_requires=REQUIRES,
-    entry_points={
-        'console_scripts': [
-            'generate-apispec=pyramid_marshmallow.scripts:generate',
-        ],
     },
 )
